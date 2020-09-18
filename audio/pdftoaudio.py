188bfc29cb1a192286ffc89c6306cbc115a01aff
omkardhangar <33811033+omkardhangar@users.noreply.github.com>
2020-09-18 13:13:12 +0530

    Update pdftoaudio.py

--- audio/pdftoaudio.py
+++ audio/pdftoaudio.py
@@ -16,33 +16,3 @@ for num in range(0, pages):
     text = page.extractText()
     play.say(text)
     play.runAndWait()
-
-
-1
-
-
-# import pyttsx3
-# import PyPDF2
-
-# book_name = open('sample.pdf', 'rb')
-# pdf_reader = PyPDF2.PdfFileReader(book_name)
-# pages = pdf_reader.numPages
-
-# play = pyttsx3.init()
-# play.setProperty('volume', 0.9)
-# play.setProperty('rate', 120)
-# print('playing....')
-
-# for num in range(0, pages):
-#     page = pdf_reader.getPage(num)
-#     text = page.extractText()
-#     play.say(text)
-#     play.runAndWait()
-
-
-# import pyttsx3
-# engine = pyttsx3.init()
-# engine.say("Omkar is my boss he is so great i have no words to describe it")
-# engine.setProperty('rate', 120)  # 120 words per minute
-# engine.setProperty('volume', 0.9)
-# engine.runAndWait()
